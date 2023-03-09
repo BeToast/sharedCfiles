cf314b6f21e8ff759fb597b2ee61bbddc3819997
Blake <note2bp@protonmail.com>
2023-03-09 17:29:43 +0000

    fixed 4 real

--- runProb2.sh
+++ runProb2.sh
@@ -2,12 +2,9 @@
 RUNS=50
 mkdir csvout/problem2
 mkdir csvout/problem2/${RUNS}runs
-# ./timer_memlock 750000 12 > csvout/problem2/${RUNS}runs/b_output.csv &
-i=0
-while [ "$i" -lt ${RUNS}]; do
-# for ((i=1; i<=$RUNS; i++))
-# do
-# ./timer_mem_use 750000 12 > csvout/problem2/${RUNS}runs/run${i}.csv &
-  ./timer_mem_use 750000 1 > csvout/problem2/${RUNS}runs/run${i}.csv &
-  i=$(( i + 1 ))
+./timer_memlock 750000 12 > csvout/problem2/${RUNS}runs/b_output.csv &
+
+for i in $(seq 50)
+do
+  ./timer_mem_use 750000 12 > csvout/problem2/${RUNS}runs/run${i}.csv &
 done
