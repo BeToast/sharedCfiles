49cb129aca3d55cf49c7b07d89578baad39a0902
blakepreston <blakepreston@brunsen64.vitrualenvs>
2023-03-11 13:20:57 +0000

    tests ran

--- runProb2.sh
+++ runProb2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-RUNS=50
+RUNS=600
 mkdir csvout
 mkdir csvout/problem2
 mkdir csvout/problem2/${RUNS}runs
-./timer_memlock 10000 12 > csvout/problem2/${RUNS}runs/b_output.csv &
+./timer_memlock 500000 12 > csvout/problem2/${RUNS}runs/b_output.csv &
 
 for i in $(seq 50)
 do
-  ./timer_mem_use 10000 12 > csvout/problem2/${RUNS}runs/run${i}.csv &
+  ./timer_mem_use 500000 12 > csvout/problem2/${RUNS}runs/run${i}.csv &
 done  
