0fb231894585d5f65f0472f92a2827d71450af2d
blakepreston <blakepreston@brunsen64.vitrualenvs>
2023-03-11 13:57:17 +0000

    fixed bug

--- runProb2.sh
+++ runProb2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-RUNS=600
+RUNS=500
 mkdir csvout
 mkdir csvout/problem2
 mkdir csvout/problem2/${RUNS}runs
-./timer_memlock 500000 12 > csvout/problem2/${RUNS}runs/b_output.csv &
-
+./timer_memlock 500000 13 > csvout/problem2/${RUNS}runs/b_output.csv &
+sleep 5
 for i in $(seq 50)
 do
   ./timer_mem_use 500000 12 > csvout/problem2/${RUNS}runs/run${i}.csv &
 done  
+sleep 125
+echo "done"
